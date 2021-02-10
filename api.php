3311aae1350a8b725cbfd8e1909f90f66092c951
keradan <daniel.kerasidi.w@gmail.com>
2021-02-10 18:31:51 +0200

    update api system

--- api.php
+++ api.php
@@ -1,15 +1,16 @@
 <?php
 
+$request = (object)[];
+$request->type = $_SERVER['REQUEST_METHOD'];
+$request->data = (object)($_POST ? $_POST : $_GET);
+
+$response = (object)[];
+$response->success = true;
+$response->request = $request;
+
 try {
 
-	$request = (object)[];
-	$request->type = $_SERVER['REQUEST_METHOD'];
-	$request->data = (object)($_POST ? $_POST : $_GET);
-	if(empty($request->data)) throw new Exception('request is empty.');
-
-	$response = (object)[];
-	$response->success = true;
-	$response->request = $request;
+	if(empty((array)$request->data)) throw new Exception('request is empty.');
 
 	header('Content-Type: application/json');
 	die(json_encode($response));
@@ -17,6 +18,7 @@ try {
 } catch (Exception $e) {
     die(json_encode([
 		'success' => false,
+		'request' => $request,
 		'error_msg' => $e->getMessage(),
 	]));
 }
--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,17 @@
-let api_url = window.location.href + 'api.php';
+function api_call(method, data, success_function = null) {
+	$.ajax({
+		url: window.location.href + 'api.php',
+		data: data,
+		dataType: 'json',
+		method: method,
+		success: success_function ?? function(response) {
+			console.log(response);
+		},
+		error: function(error){
+			console.log(error);
+		},
+	});
+}
 
-console.log('api_url: ', api_url);
-
-$.ajax({
-	url: api_url,
-	data: {
-		foo: 'bar'
-	},
-	dataType: 'json',
-	method: 'post',
-	success: function(response){
-		console.log(response);
-	},
-	error: function(error){
-		console.log(error);
-	},
-});
+let api_get = (data, success_function = null) => api_call('get', data, success_function);
+let api_post = (data, success_function = null) => api_call('post', data, success_function);
